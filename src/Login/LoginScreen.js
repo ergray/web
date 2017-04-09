7c45e6ad333e2d4251a389a22b2f23008ad844c1
David Ernst <git@dsernst.com>
2017-04-08 23:35:33 -0700

    Remove GET STARTED button from LoginScreen

--- src/Login/LoginScreen.js
+++ src/Login/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { View } from 'react-native'
 import { connect } from 'react-redux'
 import LoginHeader from './LoginHeader'
 import IntroDescription from './IntroDescription'
@@ -22,29 +22,6 @@ class LoginScreen extends Component {
         <View style={{ flex: 1, flexDirection: 'row' }}>
           <IntroDescription />
           <PhoneLoginBox loginField={loginField} navigator={this.props.navigator} />
-        </View>
-
-        <View>
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={{
-              alignItems: 'center',
-              borderColor: '#344184',
-              borderRadius: 30,
-              borderWidth: 3,
-              height: 58,
-              justifyContent: 'center',
-              marginBottom: 20,
-              marginHorizontal: 30,
-            }}
-            onPress={() => {
-              loginField.el.focus()
-            }}
-          >
-            <Text style={{ color: '#fff', fontFamily: 'HelveticaNeue', fontSize: 16, fontWeight: '600' }}>
-              GET STARTED
-            </Text>
-          </TouchableOpacity>
         </View>
 
       </View>
