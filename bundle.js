9385ed9e4d0cc6d58de2dfecdff383d2abe8a3cd
David Ernst <git@dsernst.com>
2017-04-25 09:31:14 -0700

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -14790,7 +14790,7 @@ shuffled:getShuffled};
 
 "use strict";
 Object.defineProperty(exports,"__esModule",{value:true});var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=__webpack_require__(1);var _react2=_interopRequireDefault(_react);
-var _reactNative=__webpack_require__(4);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var
+var _reactNative=__webpack_require__(4);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _objectWithoutProperties(obj,keys){var target={};for(var i in obj){if(keys.indexOf(i)>=0)continue;if(!Object.prototype.hasOwnProperty.call(obj,i))continue;target[i]=obj[i];}return target;}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var
 
 HoverableOpacity=function(_Component){_inherits(HoverableOpacity,_Component);
 function HoverableOpacity(props){_classCallCheck(this,HoverableOpacity);var _this=_possibleConstructorReturn(this,(HoverableOpacity.__proto__||Object.getPrototypeOf(HoverableOpacity)).call(this,
@@ -14799,7 +14799,7 @@ _this.state={};return _this;
 }_createClass(HoverableOpacity,[{key:'render',value:function render()
 
 {var _this2=this;var _props=
-this.props,outerStyle=_props.outerStyle,hoverStyle=_props.hoverStyle;
+this.props,outerStyle=_props.outerStyle,hoverStyle=_props.hoverStyle,remainingProps=_objectWithoutProperties(_props,['outerStyle','hoverStyle']);
 
 return(
 _react2.default.createElement(_reactNative.TouchableOpacity,{
@@ -14808,7 +14808,7 @@ style:[{cursor:'inherit'},outerStyle,this.state.hover?hoverStyle:{}],
 onMouseEnter:function onMouseEnter(){return _this2.setState({hover:true});},
 onMouseLeave:function onMouseLeave(){return _this2.setState({hover:false});}},
 
-_react2.default.createElement(_reactNative.TouchableOpacity,this.props)));
+_react2.default.createElement(_reactNative.TouchableOpacity,remainingProps)));
 
 
 }}]);return HoverableOpacity;}(_react.Component);exports.default=HoverableOpacity;
@@ -93479,7 +93479,6 @@ var _reactNative=__webpack_require__(4);
 
 
 
-
 var _reactRedux=__webpack_require__(7);
 var _reactNativeTabs=__webpack_require__(667);var _reactNativeTabs2=_interopRequireDefault(_reactNativeTabs);
 var _chevronLeft=__webpack_require__(310);var _chevronLeft2=_interopRequireDefault(_chevronLeft);
@@ -93608,7 +93607,7 @@ flexDirection:'row',
 justifyContent:'space-between'}},
 
 
-_react2.default.createElement(_reactNative.TouchableHighlight,{style:{flex:1},onPress:function onPress(){return tapPosition('yea');}},
+_react2.default.createElement(_HoverableOpacity2.default,{hoverStyle:{backgroundColor:'hsla(0,0%,100%,0.1)'},outerStyle:{flex:1},onPress:function onPress(){return tapPosition('yea');}},
 _react2.default.createElement(_reactNative.Text,{style:[{
 color:'#2ca02c',
 fontSize:16,
@@ -93619,7 +93618,7 @@ selected.yea]},'\u2713 YEA')),
 
 
 _react2.default.createElement(_reactNative.View,{style:{backgroundColor:highlightColor,width:1}}),
-_react2.default.createElement(_reactNative.TouchableHighlight,{style:{flex:1},onPress:function onPress(){return tapPosition('nay');}},
+_react2.default.createElement(_HoverableOpacity2.default,{hoverStyle:{backgroundColor:'hsla(0,0%,100%,0.1)'},outerStyle:{flex:1},onPress:function onPress(){return tapPosition('nay');}},
 _react2.default.createElement(_reactNative.Text,{style:[{
 color:'#d62728',
 fontSize:16,
@@ -93636,7 +93635,8 @@ borderBottomWidth:1,
 borderColor:highlightColor}},
 
 
-_react2.default.createElement(_reactNative.TouchableHighlight,{
+_react2.default.createElement(_HoverableOpacity2.default,{
+hoverStyle:{backgroundColor:'hsla(0,0%,100%,0.1)'},
 onPress:function onPress(){return tapPosition('abstain');}},
 
 _react2.default.createElement(_reactNative.Text,{style:[{
