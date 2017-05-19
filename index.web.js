5bb30ba92b4d6e11787dd0c467287a0a22c48330
David Ernst <git@dsernst.com>
2017-05-19 19:07:54 -0400

    Expose package.version on window.VERSION

--- index.web.js
+++ index.web.js
@@ -1,5 +1,10 @@
+/* global window */
+
 import React from 'react'
 import ReactNative from 'react-native-web'
 import App from './src/App'
+import { version } from './package.json'
+
+window.VERSION = version
 
 ReactNative.render(<App />, document.getElementById('react-app')) // eslint-disable-line no-undef
