c1da8e01aa46f86cd4e4b212bc93f12617784cbb
Alex Mingoia <talk@alexmingoia.com>
2017-08-24 11:23:21 -0700

    Use react-tappable in HoverableOpacity component
    Prevents onPress handler from being fired when scrolling.

--- src/HoverableOpacity.js
+++ src/HoverableOpacity.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import Tappable from 'react-tappable'
+import { View } from 'react-native'
 
 export default class HoverableOpacity extends Component {
   constructor(props) {
@@ -9,12 +10,14 @@ export default class HoverableOpacity extends Component {
   }
 
   render() {
-    const { children, hoverStyle, outerStyle, style, ...remainingProps } = this.props
+    const { children, hoverStyle, onPress, outerStyle, style, ...remainingProps } = this.props
 
     return (
-      <TouchableOpacity
+      <Tappable
         {...remainingProps}
         activeOpacity={0.6}
+        component="div"
+        moveThreshold={150}
         style={{
           cursor: 'pointer',
           outline: 'none',
@@ -23,9 +26,10 @@ export default class HoverableOpacity extends Component {
         }}
         onMouseEnter={() => this.setState({ hover: true })}
         onMouseLeave={() => this.setState({ hover: false })}
+        onTap={onPress}
       >
         <View style={style}>{children}</View>
-      </TouchableOpacity>
+      </Tappable>
     )
   }
 }
@@ -33,6 +37,7 @@ export default class HoverableOpacity extends Component {
 HoverableOpacity.propTypes = {
   children: PropTypes.element,
   hoverStyle: PropTypes.shape({}),
+  onPress: PropTypes.func,
   outerStyle: PropTypes.shape({}),
   style: PropTypes.shape({}),
 }
