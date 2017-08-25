72c573fd800ddbbe85f751b14ce59813f5fe6319
David Ernst <git@dsernst.com>
2017-08-24 22:58:24 -0700

    Fix /auth-error text visibility for new light bg

--- src/AuthErrorScreen.js
+++ src/AuthErrorScreen.js
@@ -9,9 +9,9 @@ function AuthErrorScreen({ dispatch, history }) {
   return (
     <View style={{ alignSelf: 'center', marginHorizontal: 30, marginTop: 20 }}>
 
-      <Text style={{ color: '#fff', fontSize: 16, marginBottom: 20 }}>It looks like your login session is invalid.</Text>
-      <Text style={{ color: '#fff', fontSize: 16, marginBottom: 20 }}>This can happen if you try to sign in multiple places.</Text>
-      <Text style={{ color: '#fff', fontSize: 16, marginBottom: 20 }}>Logging out and back in should fix it.</Text>
+      <Text style={{ fontSize: 16, marginBottom: 20 }}>It looks like your login session is invalid.</Text>
+      <Text style={{ fontSize: 16, marginBottom: 20 }}>This can happen if you try to sign in multiple places.</Text>
+      <Text style={{ fontSize: 16, marginBottom: 20 }}>Logging out and back in should fix it.</Text>
 
       <HoverableOpacity
         activeOpacity={0.5}
@@ -29,7 +29,7 @@ function AuthErrorScreen({ dispatch, history }) {
           history.replace('/')
         }}
       >
-        <Text style={{ color: '#fff', fontSize: 13 }}>
+        <Text style={{ fontSize: 13 }}>
           <LogoutIcon color="darkred" size={16} style={{ paddingBottom: 5 }} />
           &nbsp; &nbsp;LOG OUT
         </Text>
